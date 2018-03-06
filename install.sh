07fc053a459eddb413c44cfbe1f848f4ae383a46
Minory <30800424+minoryorg@users.noreply.github.com>
2018-03-06 11:05:39 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ mv laravel-resources-lang-ja/resources/lang/* ./resources/lang/
 sed -i -e "s|'locale' => 'en',|'locale' => 'ja',|g" config/app.php
 
 # Add Adminer (Database management in a single PHP file) from https://www.adminer.org/
-wget -O ./public/adminer.php https://www.adminer.org/static/download/4.3.1/adminer-4.3.1.php
+wget -O ./public/adminer.php https://www.adminer.org/static/download/4.6.2/adminer-4.6.2.php
 
 # Remove File and Folder
 rm -rf laravel
